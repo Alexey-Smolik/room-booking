01c2b7165f46d208001b4a6f38f2550698b03dda
vlgriga <vladislavgriga@gmail.com>
2018-02-27 12:41:32 +0300

    Figure out with table

--- client/src/components/BookTable.js
+++ client/src/components/BookTable.js
@@ -1,20 +1,82 @@
-import React , { Component } from 'react';
-import JqxBarGauge from 'jqwidgets-react/react_jqxbargauge.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
+ 
+//import JqxBarGauge from './assesjqwidgets-react/react_jqxbargauge.js';
+import JqxScheduler  from './assets/jqwidgets-react/react_jqxscheduler.js';
 
-// import { connect } from 'react-redux';
-
-
-class Header extends Component {
-    componentDidMount () {
-        this.refs.myScheduler.ensureAppointmentVisible('id1');
-    }
-    render () {
-         return (
-            <JqxBarGauge 
-                width={600} height={600} max={150}
-                colorScheme={'scheme02'} values={[102, 115, 130, 137]} 
-            />
-        )
-    }
+export default class BookTable extends React.Component {
+  render () {
+    let appointments = new Array();
+    let appointment1 = {
+        id: "id1",
+        description: "George brings projector for presentations.",
+        location: "",
+        subject: "Quarterly Project Review Meeting",
+        calendar: "Room 1",
+        start: new Date(2016, 10, 23, 9, 0, 0),
+        end: new Date(2016, 10, 23, 16, 0, 0)
+    };
+    let appointment2 = {
+        id: "id2",
+        description: "",
+        location: "",
+        subject: "IT Group Mtg.",
+        calendar: "Room 2",
+        start: new Date(2016, 10, 24, 10, 0, 0),
+        end: new Date(2016, 10, 24, 15, 0, 0)
+    };
+    appointments.push(appointment1);
+    appointments.push(appointment2);
+ 
+    let source =
+    {
+        dataType: "array",
+        dataFields: [
+            { name: 'id', type: 'string' },
+            { name: 'description', type: 'string' },
+            { name: 'location', type: 'string' },
+            { name: 'subject', type: 'string' },
+            { name: 'calendar', type: 'string' },
+            { name: 'start', type: 'date' },
+            { name: 'end', type: 'date' }
+        ],
+        id: 'id',
+        localData: appointments
+    };
+    let dataAdapter = new $.jqx.dataAdapter(source);
+ 
+    let resources =
+    {
+        colorScheme: "scheme05",
+        dataField: "calendar",
+        orientation: "horizontal",
+        source:  new $.jqx.dataAdapter(source)
+    };
+ 
+    let appointmentDataFields =
+    {
+        from: "start",
+        to: "end",
+        id: "id",
+        description: "description",
+        location: "place",
+        subject: "subject",
+        resourceId: "calendar"
+    };
+ 
+    let views =
+    [
+        { type: 'dayView', showWeekends: false },
+        { type: 'weekView', showWeekends: false },
+        { type: 'monthView' }
+    ];
+    return (
+        <JqxScheduler ref='myScheduler'
+            width={850} height={600} source={dataAdapter} dayNameFormat={'abbr'}
+            date={new $.jqx.date(2016, 11, 23)} showLegend={true}
+            view={'weekView'} resources={resources} views={views}
+            appointmentDataFields={appointmentDataFields}
+        />
+    )
+  }
 }
-export default Header; 
