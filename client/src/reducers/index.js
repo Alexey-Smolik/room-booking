f5a79d3bc09a2e7316b1041127f5bb28b87ccb6c
leshutic <smolikvtanke@gmail.com>
2018-03-05 17:12:27 +0300

    initial commit

--- client/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import roomsList  from './roomsList';
-import roomEvents  from './roomEvents';
-
-export default combineReducers({
-    rooms: roomsList,
-    events: roomEvents
-});
