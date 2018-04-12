cdbfbfafd69eb7bdb38bacad6d45c58337251494
leshutic <smolikvtanke@gmail.com>
2018-04-12 18:10:26 +0300

    tokens.example

---
+++ constants/tokens.exemple.js
@@ -0,0 +1,20 @@
+const tokens = {
+    vkStrategy: {
+        appId: 'appId',
+        secretKey: 'secretKey'
+    },
+    FacebookStrategy: {
+        appId: 'appId',
+        secretKey: 'secretKey'
+    },
+    TwitterStrategy: {
+        consumerKey: 'consumerKey',
+        consumerSecret: 'consumerSecret'
+    },
+    GoogleStrategy: {
+        clientID: 'clientID',
+        clientSecret: 'clientSecret'
+    }
+};
+
+module.exports = tokens;
