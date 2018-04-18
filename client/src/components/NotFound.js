c2b520915f5eda7e80296f160b4e6a5c09c38fb8
akrugliy <adept666777@gmail.com>
2018-04-18 11:19:10 +0300

    404 fix

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,25 +1,46 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Header from './header/Header';
-import Footer from './Footer/Footer';
 import {Link} from 'react-router-dom';
 
-const NotFound  = () => {
+class NotFound extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
-    return(
+    render() {
+        return (
 
-        <div className="body_404">
-            <Header />
-            <div>
-                {/*<img style={{ width: '100%', height: '80vh', backgroundSize: 'cover' }} src="http://ak7.picdn.net/shutterstock/videos/16409197/thumb/1.jpg" alt="404" />*/}
-                <h1 className="p_404">404<br/>Incorrect URL address</h1>
+            <div className="body_404">
+                <Header/>
+                {this.props.user    ?
+                    <div>
+                        <div>
+                            <h1 className="p_404">404<br/>Incorrect URL address</h1>
+                        </div>
+                        <div className="container_for_404">
+                            <Link className="link_404" to={'/room/'} title="Go home">Home</Link>
+                        </div>
+                    </div> :
+                    <div>
+                        <div>
+                            <h1 className="p_404">404<br/>Go to auth</h1>
+                        </div>
+                        <div className="container_for_404">
+                            <Link className="link_404" to={'/'} title="Go auth">Auth</Link>
+                        </div>
+                    </div>
+                }
             </div>
-            <div className="container_for_404">
-                <Link className="link_404" to={'/room/'} title="Go home">Home</Link>
-            </div>
+        );
+    }
+}
 
 
-        </div>
-    );
+ const mapStateToProps =  ({user}) =>  {
+    return {
+        user
+    }
 };
 
-export default NotFound;
+export default connect(mapStateToProps)(NotFound);
