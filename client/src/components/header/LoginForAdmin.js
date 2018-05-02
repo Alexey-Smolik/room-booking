188b930a73634716641948fd41368064b1223d27
akrugliy <adept666777@gmail.com>
2018-05-02 17:34:07 +0300

    home btn fixed

--- client/src/components/header/LoginForAdmin.js
+++ client/src/components/header/LoginForAdmin.js
@@ -6,7 +6,7 @@ const LoginForAdmin = (props) => {
     const { username } = props.user;
     return (
         <div style={{ marginBottom: '20px' }}>
-        <Link className="link_search_to_btn" to="/room" style={{ width:'104px' }}>Home</Link>
+        <Link className="link_search_to_btn" to="/room" style={{ width:'81px' }}>Home</Link>
         <form className="hello" name="search" action="#" method="get" style={{ display: 'inline-block', float: 'right' }}>
             <ControlLabel id="username_hello">Hello, {username || 'undefined'} </ControlLabel>
             <a className="link_log" href="/" onClick={() => props.logout()}>Log out</a>
